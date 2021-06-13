cd0cbee36a9bac55861279c42587b20aa172fe86
ReSide <40637990+RSide021@users.noreply.github.com>
2021-06-14 00:49:58 +0300

    Объединены проекты

--- Assets/Scripts/Plane_Move.cs
+++ Assets/Scripts/Plane_Move.cs
@@ -11,14 +11,14 @@ public class Plane_Move : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _moduleForce = 1;
     [SerializeField] private GameObject _gameOverUi;
+    [SerializeField] private Text _textScore;
     [SerializeField] private GameObject _propeller;
-    [SerializeField] private GameObject _fuelProgressBar;
     [SerializeField] private Slider _slider;
     [SerializeField] private Image _fill;
+    private int score = 0;
     private float radius;
     private float angle = 0f;
     private Vector3 defaultPosition;
-    float x0, z0, x1, z1;
     private float fuelSize = 100f;
     private float currentFuel;
     private GameObject fillGameObject;
@@ -33,28 +33,17 @@ public class Plane_Move : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(currentFuel <= 0)
+        if (currentFuel <= 0)
         {
             fillGameObject.SetActive(false);
         }
         _slider.value = currentFuel;
-        //x0 = transform.position.x;
-        //z0 = transform.position.z;
-        x1 = Mathf.Cos(angle * _speed) * radius;
-        z1 = Mathf.Sin(angle * _speed) * radius;
-        transform.position = new Vector3(x1, _plane.transform.position.y, z1);
+        var x = Mathf.Cos(angle * _speed) * radius;
+        var z = Mathf.Sin(angle * _speed) * radius;
+        transform.position = new Vector3(x, _plane.transform.position.y, z);
         angle = angle + Time.deltaTime * _speed;
         _propeller.transform.Rotate(Vector3.right, Time.deltaTime * 1000f, Space.Self);
-
         _fill.fillAmount = _slider.value;
-        //_fuelProgressBar.transform.localScale = new Vector3(currentFuel / 100, _fuelProgressBar.transform.localScale.y, _fuelProgressBar.transform.localScale.z);
-        //print(currentFuel / 100);
-        //var tan0 = z0 / x0;
-        //var tan1 = z1 / x1;
-        //var kek = Mathf.Atan((tan0 - tan1) / (1 + tan0 * tan1));
-        //transform.Rotate(Vector3.down, kek, Space.Self);
-        // поворот самолета по ссвоей оси вместо lookat (НЕРАБОТАЕТ)
-
         if (angle > 360f)
         {
             angle = 0f;
@@ -68,7 +57,7 @@ public class Plane_Move : MonoBehaviour
         {
             if (_plane.transform.position.y <= 29f)
             {
-                if(currentFuel > 0)
+                if (currentFuel > 0)
                 {
                     if (Input.GetMouseButton(0))
                     {
@@ -81,13 +70,29 @@ public class Plane_Move : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-
         if(collision.gameObject.tag == "Ground")
         {
             //_plane.transform.position = defaultPosition;
             Time.timeScale = 0f;
+            _gameOverUi.transform.GetChild(0).transform.GetChild(3).transform.GetChild(0).GetComponent<Text>().text = score.ToString();
             _gameOverUi.SetActive(true);
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cloud") {
+            //Time.timeScale = 0f;
+            other.gameObject.SetActive(false);
+            currentFuel -= 20;
+        }
+
+        if (other.gameObject.tag == "Star")
+        {
+            //Time.timeScale = 0f;
+            score += 2;
+            other.gameObject.SetActive(false);
+            _textScore.text = score.ToString();
+        }
+    }
 }
